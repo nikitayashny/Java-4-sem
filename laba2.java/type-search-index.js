e19f2b2e3b9239bba33788d42fe0395640f81915
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-02-19 12:55:29 +0300

    Delete laba2.java directory

--- laba2.java/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"by.belstu.it.yashny","l":"JavaTest"},{"p":"by.belstu.it.yashny","l":"StringWrapper"},{"p":"by.belstu.it.yashny","l":"TestClass"},{"p":"by.belstu.it.yashny","l":"TextFunction"}];updateSearchResults();
