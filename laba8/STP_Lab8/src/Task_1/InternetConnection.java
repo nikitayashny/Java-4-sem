f5774edf39ef5f5b9d107a8a276b1e0083a7cb41
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-03-30 14:35:50 +0300

    Add files via upload

---
+++ laba8/STP_Lab8/src/Task_1/InternetConnection.java
@@ -0,0 +1,107 @@
+package Task_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class InternetConnection {
+
+    public void getLocalHost() {
+        try {
+            // так можно получить адрес локального хоста (ваш айпи)
+            var currentLocalHostIP = InetAddress.getLocalHost();
+            System.out.println("Local host IP:            " + currentLocalHostIP.getHostAddress());
+        }
+        catch (UnknownHostException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    public void getByName(String web) {
+        try {
+            // получить айпишник по имени сайта (dns)
+            var pageIP = InetAddress.getByName(web);
+            System.out.println("BSTU IP by name:          " + pageIP.getHostAddress());
+        }
+        catch (UnknownHostException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    public void getByAddress(String name, byte[] ip) {
+        try {
+            // и по адресу тоже можно получить. проверяем, доступен ли 127.0.0.1
+            var pageIP = InetAddress.getByAddress(name, ip);
+            System.out.println("Is 127.0.0.1 reachable?   " + pageIP.isReachable(300));
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    public void readHTML(String urlName) {
+        try {       // еще можно весь штмл считать через команды
+            // в url закидываем адрес сайта
+            var url = new URL(urlName);
+            // через буферизированный поток все читаем построчно
+            try (var reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                String HTMLLine;
+                while ((HTMLLine = reader.readLine()) != null) {
+                    System.out.println(HTMLLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        catch(MalformedURLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    // можно еще всякую разную инфу о соединении получить
+    public void getInfo(String urlName) {
+        var timeout = 10_000;
+        try {
+            var url = new URL(urlName);
+            // создали объект коннекшена к какому то сайту из параметров функции
+            final var connection = url.openConnection();
+
+            System.out.println("Content type:     " + connection.getContentType() +     // тип контента (штмл страница)
+                             "\nClass:            " + connection.getClass() +           // класс
+                             "\nContent length:   " + connection.getContentLength());   // длина контента
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ laba8/STP_Lab8/src/Task_3/SendUDP.java
@@ -0,0 +1,31 @@
+package Task_3;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class SendUDP {
+    // точно так же юзаем сокеты для подключения, но щас юзаем класс DatagramSocket
+    private DatagramSocket socket;
+
+
+    public SendUDP(int port) throws SocketException {
+        // один параметр перегруженной ф-ии,значит коннектимся к определенному порту на локальной машине
+        socket = new DatagramSocket(port);
+    }
+
+
+    public void sendMessages(String messages, int port) throws IOException {
+        // ф-ия для отправки сообщения,тут все просто
+        DatagramPacket message = new DatagramPacket(messages.getBytes(), messages.getBytes().length, InetAddress.getLocalHost(), port);
+        socket.send(message);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        SendUDP udp = new SendUDP(1234);
+        // попытаемся отправить это сообщение
+        udp.sendMessages("всем привет", 4321);
+    }
+}
